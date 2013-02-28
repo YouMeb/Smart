c4788e5627351f4e6a54fa8e68e91d76331453ba
iamblue <blue.chen@kindminds.com>
2013-02-28 13:12:17 +0800

    test

--- app.js
+++ app.js
@@ -41,6 +41,9 @@ app.get('/1_0', function(req, res) {
 app.get('/6_0', function(req, res) {
   res.render('6_0');
 });
+app.get('/6_1', function(req, res) {
+  res.render('6_1');
+});
 app.get('/7_0', function(req, res) {
   res.render('7_0');
 });
