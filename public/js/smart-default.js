3a0ce542fc1bb61174b8c789f608c8d53d5c16c3
iamblue <blue.chen@kindminds.com>
2013-03-01 16:44:31 +0800

    test

--- public/js/smart-default.js
+++ public/js/smart-default.js
@@ -23,7 +23,74 @@ $(function() {
 			$('.megamenu').css('display', 'none');
 			$showadframe.removeClass('active');
 		});
-		$(this).addClass('checktime');
+		$('.megamenu').hide();
+		var section = $(this).data('section');
+		if ($(this).hasClass('checktime')){
+			
+			switch(section) {
+			case 'aa':
+				$('.megamenu_' + section).show().addClass('fadeIn');
+				if($('#drop1').hasClass('checktime')) {
+					console.log('yoyo');
+					location.href = '/test/tpl/dd1.php'; // 請輸入同學會Url
+				}
+				break;
+			case 'class':
+				$('.megamenu_' + section).show().addClass('fadeIn');
+				if($('#drop2').hasClass('checktime')) {
+					location.href = '/test/tpl/dd2.php'; //請輸入課程Url
+				}
+				break;
+			case 'DVD':
+				$('.megamenu_' + section).show().addClass('fadeIn');
+				if($('#drop3').hasClass('checktime')) {
+					location.href = '/test/tpl/dd3.php'; //請輸入DVD Url
+				}
+				break;
+			case 'magazine':
+				$('.megamenu_' + section).show().addClass('fadeIn');
+				if($('#drop4').hasClass('checktime')) {
+					location.href = '/test/tpl/dd4.php'; //請輸入雜誌Url
+				}
+				break;
+			case 'book':
+				location.href = '/test/tpl/dd.php'; //請輸入叢書Url
+				break;
+			case 'forum':
+				location.href = '/test/tpl/dd.php'; //請輸入討論區Url
+				break;
+			case 'purchase':
+				location.href = '/test/tpl/dd.php'; //請輸入限時搶購Url
+				break;
+			}
+
+		}else{
+			switch(section) {
+			case 'aa':
+				$('.megamenu_' + section).show().addClass('fadeIn');
+				break;
+			case 'class':
+				$('.megamenu_' + section).show().addClass('fadeIn');
+				break;
+			case 'DVD':
+				$('.megamenu_' + section).show().addClass('fadeIn');
+				break;
+			case 'magazine':
+				$('.megamenu_' + section).show().addClass('fadeIn');
+				break;
+			case 'book':
+				location.href = '/test/tpl/dd.php'; //請輸入叢書Url
+				break;
+			case 'forum':
+				location.href = '/test/tpl/dd.php'; //請輸入討論區Url
+				break;
+			case 'purchase':
+				location.href = '/test/tpl/dd.php'; //請輸入限時搶購Url
+				break;
+			}
+			$(this).addClass('checktime');
+			
+		}
 		document.getElementById('showaddiv1').style.display = 'block';
 		$showadframe.removeClass('fadeOut').addClass('animated').addClass('fadeIn');
 		var section = $(this).data('section');
@@ -31,7 +98,7 @@ $(function() {
 		switch(section) {
 		case 'aa':
 			$('.megamenu_' + section).show().addClass('fadeIn');
-			if($('.megamenu_' + section).hasClass('checktime')) {
+			if($('#drop1').hasClass('checktime')) {
 				console.log('yoyo');
 				location.href = '/test/tpl/dd1.php'; // 請輸入同學會Url
 			}
