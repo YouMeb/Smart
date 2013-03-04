fef094d4de5a4fce8880aad72778f8d49ce60fe5
iamblue <blue.chen@kindminds.com>
2013-03-04 12:11:21 +0800

    test

--- public/js/smart-default.js
+++ public/js/smart-default.js
@@ -28,11 +28,17 @@ $(function() {
 		if ($(this).hasClass('checktime')){
 			
 			switch(section) {
+			case 'aab':
+				$('.megamenu_' + section).show().addClass('fadeIn');
+				if($('#drop1').hasClass('checktime')) {
+					console.log('go!!');
+					//location.href = '/test/tpl/dd2.php'; //請輸入課程Url
+				}
+				break;
 			case 'aa':
 				$('.megamenu_' + section).show().addClass('fadeIn');
 				if($('#drop1').hasClass('checktime')) {
-					console.log('yoyo');
-					location.href = '/test/tpl/dd1.php'; // 請輸入同學會Url
+					location.href = '/test/tpl/dd2.php'; //請輸入課程Url
 				}
 				break;
 			case 'class':
@@ -91,46 +97,7 @@ $(function() {
 			$(this).addClass('checktime');
 			
 		}
-		document.getElementById('showaddiv1').style.display = 'block';
-		$showadframe.removeClass('fadeOut').addClass('animated').addClass('fadeIn');
-		var section = $(this).data('section');
-		$('.megamenu').hide();
-		switch(section) {
-		case 'aa':
-			$('.megamenu_' + section).show().addClass('fadeIn');
-			if($('#drop1').hasClass('checktime')) {
-				console.log('yoyo');
-				location.href = '/test/tpl/dd1.php'; // 請輸入同學會Url
-			}
-			break;
-		case 'class':
-			$('.megamenu_' + section).show().addClass('fadeIn');
-			if($('.megamenu_' + section).hasClass('checktime')) {
-				location.href = '/test/tpl/dd2.php'; //請輸入課程Url
-			}
-			break;
-		case 'DVD':
-			$('.megamenu_' + section).show().addClass('fadeIn');
-			if($('.megamenu_' + section).hasClass('checktime')) {
-				location.href = '/test/tpl/dd3.php'; //請輸入DVD Url
-			}
-			break;
-		case 'magazine':
-			$('.megamenu_' + section).show().addClass('fadeIn');
-			if($('.megamenu_' + section).hasClass('checktime')) {
-				location.href = '/test/tpl/dd4.php'; //請輸入雜誌Url
-			}
-			break;
-		case 'book':
-			location.href = '/test/tpl/dd.php'; //請輸入叢書Url
-			break;
-		case 'forum':
-			location.href = '/test/tpl/dd.php'; //請輸入討論區Url
-			break;
-		case 'purchase':
-			location.href = '/test/tpl/dd.php'; //請輸入限時搶購Url
-			break;
-		}
+		
 	});
 	//首頁選單結束
 
